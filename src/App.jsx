558df29a3013b9db640281ae0a6ea1cd715ff854
Aaron MV <epsilon11101@gmail.com>
2023-06-01 10:01:42 -0600

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
-import React from 'react';
-import BackwardCounter from './components/BackwardCounter';
-import ForwardCounter from './components/ForwardCounter';
-
 function App() {
-  return (
-    <React.Fragment>
-      <ForwardCounter />
-      <BackwardCounter />
-    </React.Fragment>
-  );
+  return <div></div>;
 }
 
 export default App;
